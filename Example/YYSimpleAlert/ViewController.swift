224ffe55d963f5ba7f3087c0f3a524095e22fb00
ash-youu <ash.you.dev@gmail.com>
2022-10-31 20:39:47 +0900

    Initial commit

---
+++ Example/YYSimpleAlert/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  YYSimpleAlert
+//
+//  Created by ash.you.dev@gmail.com on 10/31/2022.
+//  Copyright (c) 2022 ash.you.dev@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
